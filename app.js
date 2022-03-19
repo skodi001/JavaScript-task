7bd21b7a78f277eaf4ecbae08c1a9e8690a5089a
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-19 21:27:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function isValidPassword(password, username){
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1;
+    const tooSimilar = password.indexOf(username) !== -1;
+    if (tooShort || hasSpace || tooSimilar) return false;
+    return true;
+}
