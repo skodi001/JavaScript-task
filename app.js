edd68d5b9df7fb32bbae4790bf6d67d156bec8e0
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-19 21:49:08 +0100

    isValidPassword function is created

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function isValidPassword(password, username){
-    const tooShort = password.length < 8;
-    const hasSpace = password.indexOf(' ') !== -1;
-    const tooSimilar = password.indexOf(username) !== -1;
-    if (tooShort || hasSpace || tooSimilar) return false;
-    return true;
+function isValidPassword(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(" ") !== -1;
+  const tooSimilar = password.indexOf(username) !== -1;
+  if (tooShort || hasSpace || tooSimilar) return false;
+  return true;
 }
-console.log("hello world");
+console.log("Hello World Shokhmurod");
